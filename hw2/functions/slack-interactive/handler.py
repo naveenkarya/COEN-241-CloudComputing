94cdbc5c9d907c5354a978d49d724353f0bc4b5a
naveenkarya <naveenkarya@github.com>
2022-05-05 16:00:26 -0700

    code and screenshots for hw2

---
+++ hw2/functions/slack-interactive/handler.py
@@ -0,0 +1,30 @@
+import json
+import urllib
+import logging
+logging.basicConfig(level=logging.DEBUG)
+def handle(req):
+    urlstring = urllib.unquote(req).decode('utf8').strip('payload=')
+    request = json.loads(urlstring)
+    data = {
+        "attachments": [
+            {
+                "replace_original": True,
+                "response_type": "ephemeral",
+                "fallback": "Required plain-text summary of the attachment.",
+                "color": "#36a64f",
+                "pretext": "Ahh yeah! Great choice, COEN 241 is absolutely amazing!",
+                "author_name": request.get("author", "Naveen Kumar"),
+                "author_link": "https://github.com/naveenkarya",
+                "author_icon": "https://avatars.githubusercontent.com/u/28781654?s=400&u=e62923e4d3784d79a35208b4f4cb58567c6ac72f&v=4",
+                "title": "COEN 241",
+                "title_link": "https://www.scu.edu/engineering/academic-programs/department-of-computer-engineering/graduate/course-descriptions/",
+                "text": "Head over to COEN 241",
+                "image_url": "https://www.scu.edu/media/offices/umc/scu-brand-guidelines/visual-identity-amp-photography/visual-identity-toolkit/logos-amp-seals/Mission-Dont3.png",
+                "thumb_url": "https://www.scu.edu/engineering/academic-programs/department-of-computer-engineering/graduate/course-descriptions/",
+                "footer": "Slack Apps built on OpenFaas",
+                "footer_icon": "https://a.slack-edge.com/45901/marketing/img/_rebrand/meta/slack_hash_256.png",
+                "ts": 123456789
+            }
+        ]
+    }
+    return json.dumps(data)
---
+++ hw2/screenshots/README.txt
@@ -0,0 +1,8 @@
+Description of Screenshots
+
+invoke-figlet.png: Execution of figlet function.
+journalctl.png: faasd logs using journalctl.
+openfaas-gateway.png: Gateway UI after deploying all the functions.
+slack-request.png: Execution of slack-request function.
+slack-interactive.png: Execution of slack-interactive function.
+author-name-from-input.png: Taking author name from input request instead of hardcoding it.
